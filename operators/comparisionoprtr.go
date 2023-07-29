2bcedf70c36fc2f1be67e431a457a6a541f48e2a
vamshi1188 <saivamshik11@gmail.com>
2023-07-29 18:33:35 +0530

    added comparison file

---
+++ operators/comparisionoprtr.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a < b)
+	fmt.Println(a <= b)
+	fmt.Println(a > b)
+	fmt.Println(a >= b)
+
+}
