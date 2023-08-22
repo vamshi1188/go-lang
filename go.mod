137745a135e3d3077cb17ba9c5695b2ab1398904
vamshi <saivamshik11@gmail.com>
2023-08-23 00:46:58 +0530

    fjnd

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/vamshi1188/go-lang
-
-go 1.20
--- typecasting.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var num int = 10
-	var points float64 = float64(num)
-	fmt.Printf("%.2f\n", points)
-}
