aadcb6c6a05f245ff1482a1c3b8bfa16d7dddf72
vamshi <saivamshik11@gmail.com>
2023-08-23 14:52:33 +0530

    hvx

--- Applications/Booking_app.go
+++ Applications/Booking_app.go
@@ -9,4 +9,22 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v  are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets to attend ")
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets string
+	// ask for user
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter no of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for bokking %v tickets. you will get a confirmation mail on %v \n", firstName, lastName, userTickets, email)
 }
