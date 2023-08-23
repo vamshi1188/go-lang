1a3ba162d672ab201b9972072d0a87b720f20f79
vamshi <saivamshik11@gmail.com>
2023-08-23 14:51:56 +0530

    added some code

--- Applications/Booking_app.go
+++ Applications/Booking_app.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hellos")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v  are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets to attend ")
 }
---
+++ Applications/go.mod
@@ -0,0 +1,3 @@
+module Booking_app
+
+go 1.20
